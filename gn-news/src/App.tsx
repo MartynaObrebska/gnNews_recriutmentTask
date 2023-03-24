c384e63114b40e7298149e4aba31b12ca6b26687
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-03-24 14:03:28 +0100

    Add basic structure of an app.

--- gn-news/src/App.tsx
+++ gn-news/src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
+import SideMenu from "./components/SideMenu/SideMenu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <SideMenu />
+      <div className="content">
+        <MainContent />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
