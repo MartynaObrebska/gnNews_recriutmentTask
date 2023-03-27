f7d0370ee239a05ecd6cf90aeee4f7da00fc1118
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-03-27 15:53:06 +0200

    Add mapping of countries.

--- gn-news/src/components/SideMenu/SideMenu.tsx
+++ gn-news/src/components/SideMenu/SideMenu.tsx
@@ -1,17 +1,30 @@
 import { MenuList, MenuItem, ListItemText } from "@mui/material";
 
 function SideMenu() {
+  const countries = [
+    "Country placeholder",
+    "Country placeholder",
+    "Country placeholder",
+    "Country placeholder",
+    "Country placeholder",
+    "Country placeholder",
+    "Country placeholder",
+  ];
   return (
     <MenuList>
-      <MenuItem>
-        <ListItemText>Country placeholder</ListItemText>
-      </MenuItem>
-      <MenuItem>
-        <ListItemText>Country placeholder</ListItemText>
-      </MenuItem>
-      <MenuItem>
-        <ListItemText>Country placeholder</ListItemText>
-      </MenuItem>
+      {countries.map((item, index) => (
+        <MenuItem
+          key={index}
+          sx={{
+            bgcolor: "transparent",
+            "&:hover": {
+              backgroundColor: "primary.main",
+            },
+          }}
+        >
+          <ListItemText>{item}</ListItemText>
+        </MenuItem>
+      ))}
     </MenuList>
   );
 }
