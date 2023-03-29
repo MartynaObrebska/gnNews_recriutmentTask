ef73b65f4743b243433c3a2bfd981be8813d2c5c
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-03-29 14:57:37 +0200

    Styling.

--- gn-news/src/components/Footer/Footer.tsx
+++ gn-news/src/components/Footer/Footer.tsx
@@ -2,19 +2,21 @@ import { Grid, Typography } from "@mui/material";
 
 function Footer() {
   return (
-    <Grid
-      container
-      sx={{ height: "84px", color: "white", backgroundColor: "#3a424d" }}
-    >
-      <Grid xs={2}>
-        <Typography align="center" sx={{ lineHeight: "84px" }}>
-          Time placeholder
-        </Typography>
-      </Grid>
-      <Grid xs={10}>
-        <Typography align="center" sx={{ lineHeight: "84px" }}>
-          Total articles:
-        </Typography>{" "}
+    <Grid item xs={12} sx={{ height: "84px" }}>
+      <Grid
+        container
+        sx={{ height: "84px", color: "white", backgroundColor: "#3a424d" }}
+      >
+        <Grid item xs={2}>
+          <Typography align="center" sx={{ lineHeight: "84px" }}>
+            Time placeholder
+          </Typography>
+        </Grid>
+        <Grid item xs={10}>
+          <Typography align="center" sx={{ lineHeight: "84px" }}>
+            Total articles:
+          </Typography>{" "}
+        </Grid>
       </Grid>
     </Grid>
   );
--- gn-news/src/components/Header/Header.tsx
+++ gn-news/src/components/Header/Header.tsx
@@ -1,33 +1,35 @@
 import ToggleViewButtons from "./ToggleViewButtons/ToggleViewButtons";
 import logo from "../../assets/gnNews.png";
-import { AppBar, Box, Button, Stack, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, Grid, Stack, Toolbar } from "@mui/material";
 
 function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="fixed"
-        color={"secondary"}
-        justify-content="space-between"
-      >
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }}>
-            <Box component="a" href="/">
-              <img src={logo} alt="gnNews" loading="lazy" />
+    <Grid item xs={12} sx={{ height: "84px" }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="fixed"
+          color={"secondary"}
+          justify-content="space-between"
+        >
+          <Toolbar>
+            <Box sx={{ flexGrow: 1 }}>
+              <Box component="a" href="/">
+                <img src={logo} alt="gnNews" loading="lazy" />
+              </Box>
             </Box>
-          </Box>
-          <Stack spacing={2} direction="row" sx={{ padding: "0 30px" }}>
-            <ToggleViewButtons />
-            <Button
-              variant="contained"
-              sx={{ fontWeight: "800", paddingTop: "9px" }}
-            >
-              Click!
-            </Button>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Stack spacing={2} direction="row" sx={{ padding: "0 30px" }}>
+              <ToggleViewButtons />
+              <Button
+                variant="contained"
+                sx={{ fontWeight: "800", paddingTop: "9px" }}
+              >
+                Click!
+              </Button>
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Grid>
   );
 }
 
