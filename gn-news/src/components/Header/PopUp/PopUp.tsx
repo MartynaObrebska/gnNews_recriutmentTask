b6ff56058452d4e60a9850e513a2f621504e0ab4
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-03-31 14:01:25 +0200

    Add content.

--- gn-news/src/components/Header/PopUp/PopUp.tsx
+++ gn-news/src/components/Header/PopUp/PopUp.tsx
@@ -22,18 +22,31 @@ export default function PopUp(props: Props) {
   return (
     <Modal open={open} onClose={handleClose}>
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Największa frajda
+        <Typography
+          align="center"
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          sx={{ marginBottom: 2 }}
+        >
+          Największe trudności i przyjemności
         </Typography>
-        <Typography id="modal-modal-title" variant="body1" component="h2">
-          Placeholder
-        </Typography>
-        <br />
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Największe trudności
-        </Typography>
-        <Typography id="modal-modal-title" variant="body1" component="h2">
-          Placeholder
+        <Typography
+          align="justify"
+          id="modal-modal-title"
+          variant="body1"
+          component="h2"
+        >
+          Największą trudność sprawiła mi praca z biblioteką UI i Reduxem,
+          ponieważ nie miałam wcześniej zbyt dużego doświadczenia z tymi
+          narzędziami. Staram sie jednak traktować to bardziej jako wyzwania nie
+          problem, ponieważ dzięki temu bardziej się rozwijam i uczę nowych
+          rzeczy. Daje mi to zdecydowanie wiekszą satysfakcję, gdy uda mi się
+          pokonać jakąś trudność. Dlatego właśnie te same aspekty były dla mnie
+          również największą przyjemnością. Ciekawa była również współpraca z
+          News API. Duuuużo frajdy sprawiło mi także projektowanie designu całej
+          aplikacji w oparciu o design strony gnStudio. Ogólnie praca nad
+          projektem była bardzo satysfakcjonująca i rozwijająca.
         </Typography>
       </Box>
     </Modal>
